ec4fc4cf50c95bf9721102b5672ec39415acd2ac
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-06-21 17:06:19 +0200

    minor

--- src/python/dxpy/nextflow_builder.py
+++ src/python/dxpy/nextflow_builder.py
@@ -2,14 +2,6 @@ import os
 
 NF_PATH_LOCAL = ".dx_nextflow/"
 DXAPP = ".dx_nextflow/dxapp.json"
-EXEC = ".dx_nextflow/nextflow.sh"
-EXEC_CONTENT = '''
-#!/usr/bin/env bash
-curl -s https://get.nextflow.io | bash
-mv nextflow /usr/bin
-
-nextflow run /
-'''
 DXAPP_CONTENT = '''
 {
   "name": "nextflow",
@@ -58,12 +50,39 @@ DXAPP_CONTENT = '''
   "ignoreReuse": true
 }
 '''
-def prepare_nextflow(args):
-    print(args._repository)
-    print(args._tag)
-    if not os.path.exists(NF_PATH_LOCAL):
-        os.makedirs(NF_PATH_LOCAL)
+
+def write_exec(repo, tag):
+    GCLONE_PLACEHOLDER = "@GIT_CLONE_PLACEHOLDER"
+    GCHECKOUT_PLACEHOLDER = "@GIT_CHECKOUT_PLACEHOLDER"
+    EXEC = ".dx_nextflow/nextflow.sh"
+    exec_content = f'''
+        #!/usr/bin/env bash
+        curl -s https://get.nextflow.io | bash
+        mv nextflow /usr/bin
+        {GCLONE_PLACEHOLDER}
+        {GCHECKOUT_PLACEHOLDER}
+        nextflow run /
+    '''
+    g_clone = ""
+    g_checkout = ""
+    if repo:
+        g_clone = f"git clone {repo} /"
+        if tag:
+            g_checkout = f'''
+            pushd /
+            git checkout {tag}
+            popd
+            '''
+    exec_content.replace(GCLONE_PLACEHOLDER, g_clone)
+    exec_content.replace(GCHECKOUT_PLACEHOLDER, g_checkout)
+    with open(EXEC, "w") as exec:
+        exec.write(exec_content)
+
+def write_dxapp():
     with open(DXAPP, "w") as dxapp:
         dxapp.write(DXAPP_CONTENT)
-    with open(EXEC, "w") as exec:
-        exec.write(EXEC_CONTENT)
+def prepare_nextflow(args):
+    if not os.path.exists(NF_PATH_LOCAL):
+        os.makedirs(NF_PATH_LOCAL)
+    write_dxapp()
+    write_exec(args._repository, args._tag)
