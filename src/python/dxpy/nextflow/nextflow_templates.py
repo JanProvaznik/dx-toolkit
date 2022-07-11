d8c7e6809c1f266d6284763e6a5ddff1e74e7ea4
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-07-11 16:53:04 +0200

    minor nf

--- src/python/dxpy/nextflow/nextflow_templates.py
+++ src/python/dxpy/nextflow/nextflow_templates.py
@@ -9,6 +9,62 @@ def get_nextflow_dxapp():
         "billTo": "org-dnanexus_apps",
         "version": "1.0.0",
         "inputSpec": [
+        {
+            "name": "nf_run_args_and_pipeline_params",
+            "label": "Nextflow Run Arguments and Pipeline Parameters",
+            "help": "Additional run arguments and pipeline parameters for Nextflow (i.e. -queue-size).",
+            "class": "string",
+            "optional": true
+        },
+        {
+            "name": "resume",
+            "label": "Resume",
+            "help": "Enables resume functionality in Nextflow workflows.",
+            "class": "boolean",
+            "default": false
+        },
+        {
+            "name": "resume_session",
+            "label": "Resume Session",
+            "help": "Session or job to be resumed.",
+            "class": "string",
+            "optional": true
+        },
+        {
+            "name": "nf_advanced_opts",
+            "label": "Nextflow Advanced Options",
+            "help": "Advanced options for Nextflow (i.e. -quiet).",
+            "class": "string",
+            "optional": true
+        },
+        {
+            "name": "docker_creds",
+            "label": "Docker Credentials",
+            "help": "Docker Credentials used to obtain private docker images.",
+            "class": "file",
+            "optional": true
+        },
+        {
+            "name": "debug",
+            "label": "Debug Mode",
+            "help": "Shows additional information in Nextflow logs.",
+            "class": "boolean",
+            "default": false
+        },
+        {
+            "name": "secret_directive_file",
+            "label": "Secret Directive File",
+            "help": "Adds the built-in Nextflow support for pipeline secrets to allow users to handle and manage sensitive information for pipeline execution in a safe manner.",
+            "class": "file",
+            "optional": true
+        },
+        {
+            "name": "no_future_resume",
+            "label": "No Future Resume",
+            "help": "Allow saving workspace and cache files to the platform to be used later for resume functionality.",
+            "class": "boolean",
+            "default": false
+        },
         ],
         "outputSpec": [
         ],
@@ -45,6 +101,7 @@ def get_nextflow_dxapp():
             "allProjects": "VIEW"
         }
     }'''
+
 
 def get_nextflow_src():
     return '''
